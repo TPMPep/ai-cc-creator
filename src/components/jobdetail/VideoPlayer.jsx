a92274411483e90e6de9b0a13caf8ddcfd84105b
Paulette Pantoja <directrpep@gmail.com>
2026-02-12 17:04:40 +0000

    Job table improvements

--- src/components/jobdetail/VideoPlayer.jsx
+++ src/components/jobdetail/VideoPlayer.jsx
@@ -67,6 +67,7 @@ export default function VideoPlayer({ mediaUrl, cues, videoRef, onTimeUpdate, ca
         controls
         className="w-full aspect-video bg-black"
         onTimeUpdate={handleTimeUpdate}
+        crossOrigin="anonymous"
       />
       {/* Caption overlay */}
       {activeCue && (
