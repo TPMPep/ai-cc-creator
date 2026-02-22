6ed14b2564ffe830a414ef15bbc154528ede1858
Paulette Pantoja <directrpep@gmail.com>
2026-02-22 10:10:47 +0000

    File changes

--- src/pages/DiagnosticAI.jsx
+++ src/pages/DiagnosticAI.jsx
@@ -76,6 +76,7 @@ export default function DiagnosticAI() {
 
   const finalCues = job.result?.cues || [];
   const assemblyCues = job.result?.assemblyRawCues || job.result?.diagnostic?.assemblyRawCues || [];
+  const assemblyUtterances = job.result?.assemblyUtterances || [];
   const openaiCues = job.result?.openaiReformattedCues || job.result?.diagnostic?.openaiRawCues || [];
   const rows = alignRows(assemblyCues, openaiCues, finalCues);
 
