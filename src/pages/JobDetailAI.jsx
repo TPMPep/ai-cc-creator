709c47858a9628dc06a53a458be933f98a6656ec
Paulette Pantoja <directrpep@gmail.com>
2026-02-24 13:12:54 +0000

    File changes

--- src/pages/JobDetailAI.jsx
+++ src/pages/JobDetailAI.jsx
@@ -14,7 +14,7 @@ import { Input } from "@/components/ui/input";
 import { Loader2, Plus, RefreshCw, AlertCircle, Pencil, Check, X, Sparkles, FlaskConical, RotateCcw } from "lucide-react";
 
 import PipelineLog from "../components/jobdetail/PipelineLog";
-import { getCuesFromResult } from "../components/shared/CueUtils";
+import { getCuesFromResult, getCuesFromResultAsync } from "../components/shared/CueUtils";
 import moment from "moment";
 import { toast } from "sonner";
 
